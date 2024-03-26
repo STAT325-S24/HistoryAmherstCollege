ff060c4d3cea0e8d6d6b873b17432211faf6c3e8
arogers24 <arogers24@amherst.edu>
2024-03-26 15:57:08 -0400

    Fix punctuation issue

--- data-raw/dehyphenate.R
+++ data-raw/dehyphenate.R
@@ -5,7 +5,7 @@ fix_up_lines <- function(line) {
   for (i in 1 : n) {
     line[i] <- str_trim(line[i])
     if (str_detect(line[i], "-$")) {
-      padding <- str_match(line[i+1], "[\\w\\d]+[:punct: ]?")
+      padding <- str_match(line[i+1], "[\\w+\\d]+[:punct:]?")
       line[i] <- paste(str_sub(line[i], start=1, end=-2), padding, sep="")
       line[i] <- str_trim(line[i])
       line[i+1] <- str_replace(line[i+1], padding, "")
@@ -14,19 +14,6 @@ fix_up_lines <- function(line) {
   #print(line)
   return(line)
 }
-
-# fix_up_lines <- function(line) {
-#   num_lines <- length(line)
-#   line2 <- str_squish(line)
-#   for (i in 1:(num_lines-1)){
-#     if (str_detect(line2[i], "-$")){
-#       line2[i] <- str_replace(line2[i], "-$", "")
-#       line2[i] <- paste0(line2[i], str_extract(line2[i+1], "\\w+[ :punct:]?"))
-#       line2[i+1] <- str_replace(line2[i+1], str_extract(line2[i+1], "\\w+[ :punct:]?"), "")
-#     }
-#   }
-#   return(line2)
-# }
 
 path <- getwd()
 my_txt <- readLines(paste(path, "/data-raw/chapter10.txt", sep = "")) 
