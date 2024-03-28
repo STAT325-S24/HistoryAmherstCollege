86fee17332ad2bcee8cff27e4c77cca86384f1eb
jpapagelis24 <jpapagelis24@amherst.edu>
2024-03-28 16:29:43 -0400

    Added the page tables and the depaginated pages in new folders

--- data-raw/wrangle_page_breaks.R
+++ data-raw/wrangle_page_breaks.R
@@ -1,38 +1,65 @@
 library(tidyverse)
-chap01 <- readLines("data-raw/chapter01.txt")
-total_lines <- length(chap01)
-total_lines
+library(stringr)
 
-page_table <- tibble(
-  page_number = integer(),
-  page_header = character(),
-  first_line = character()
-)
 
-curr_chap <- chap01
-for(i in 1:(total_lines - 2)) {
-  if (str_detect(curr_chap[i], "^ $") && 
-      str_detect(curr_chap[i+1], "^ $") && 
-      str_detect(curr_chap[i+2], "^ $") && 
-      str_detect(curr_chap[i+3], "^ $") == FALSE &&
-      str_detect(curr_chap[i+4], "^ $"))
-    {
-   print(i)
-    
-    curr_first_line <- curr_chap[i+5]
-    curr_page_header <- curr_chap[i+3]
-    curr_page_number <- str_extract(curr_chap[i+3], "[:digit:]+")
-    
-    page_table <- rbind(page_table, tibble(
-      page_number = curr_page_number,
-      page_header = curr_page_header,
-      first_line = curr_first_line
-    ))
-    
+process_chapter_data <- function(chapter_name) {
+  chap_data <- readLines(paste0("data-raw/", chapter_name, ".txt"))
+  total_lines <- length(chap_data)
+  
+  page_table <- tibble(
+    page_number = integer(),
+    page_header = character(),
+    first_line = character()
+  )
+  
+  for(i in 1:(total_lines - 5)) {
+    if (str_detect(chap_data[i], "^ $") && 
+        str_detect(chap_data[i+1], "^ $") && 
+        str_detect(chap_data[i+2], "^ $") && 
+        str_detect(chap_data[i+3], "^ $") == FALSE &&
+        str_detect(chap_data[i+4], "^ $")) {
+      
+      curr_first_line <- chap_data[i+5]
+      curr_page_header <- chap_data[i+3]
+      
+      if(str_detect(chap_data[i+3], "(?<=\\.\\s)\\d+")) {
+        curr_page_number <- as.integer(str_extract(chap_data[i+3], "(?<=\\.\\s)\\d+"))
+      } else {
+        curr_page_number <- as.integer(str_extract(chap_data[i+3], "\\d+"))
+      }
+      
+      page_table <- bind_rows(page_table, tibble(
+        page_number = curr_page_number,
+        page_header = curr_page_header,
+        first_line = curr_first_line
+      ))
+    }
   }
+
+  saveRDS(page_table, paste0("page-tables/page_table_", gsub("data-raw/", "", gsub(".txt", "", chapter_name)), ".rds"))
+  
+  
+  con <- file(paste0("data-raw-depaginate", "/", gsub("data-raw/", "", gsub(".txt", "", chapter_name)), "_cleaned.txt"), open = "w")
+  i <- 1
+  while(i <= (total_lines - 5)) {
+    if (str_detect(chap_data[i+1], "^ $") && 
+        str_detect(chap_data[i+2], "^ $") && 
+        str_detect(chap_data[i+3], "^ $") && 
+        str_detect(chap_data[i+4], "^ $") == FALSE &&
+        str_detect(chap_data[i+5], "^ $")) {
+      i <- i + 5
+    } else {
+      writeLines(chap_data[i], con)
+      i <- i + 1
+    }
+  }
+  close(con)
 }
 
-page_table
 
-
-chap01
+for(i in 0:28) {
+  chapter_num <- sprintf("%02d", i)
+  chapter_name <- paste("chapter", chapter_num, sep="")
+  
+  process_chapter_data(chapter_name)
+}
