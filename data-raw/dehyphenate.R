9831becd5a95a0fdf3b98e1de73f5353790afed7
arogers24 <arogers24@amherst.edu>
2024-04-02 16:18:02 -0400

    Move wrangling files to data-raw

--- data-raw/dehyphenate.R
+++ data-raw/dehyphenate.R
@@ -1,12 +1,9 @@
 library(stringr)
 
 fix_up_lines <- function(file) {
-  # file <- "chapter10_cleaned.txt"
-  path <- getwd()
-  chapter_lines <- readLines(paste(path, "/data-raw-depaginate/", file, sep = "")) 
+  chapter_lines <- readLines(file) 
   n <- length(chapter_lines)
   for (i in 1 : n) {
-    # i <- 1337
     chapter_lines[i] <- str_trim(chapter_lines[i])
     if (str_detect(chapter_lines[i], "-$")) {
       if (str_trim(chapter_lines[i+1]) == "") {
@@ -23,19 +20,15 @@ fix_up_lines <- function(file) {
       }
     }
   }
-  #print(line)
   return(chapter_lines)
 }
 
 path <- getwd()
 for(i in 0:28) {
   chapter_num <- sprintf("%02d", i)
-  chapter_name <- paste(path, "/data-raw-depaginate/", chapter_num, "_cleaned.txt", sep = "")
+  chapter_name <- paste(path, "/data-raw-depaginate/chapter", chapter_num, "_cleaned.txt", sep = "")
   
-  # table <- process_chapter_data(chapter_name)
-  
-  print(chapter_name)
-  # print(table, n = 100)
-  
+  table <- fix_up_lines(chapter_name)
+  write.table(table, paste0(path, "/data-raw-dehyphenate/chapter", chapter_num, "_dehyphenate.txt", sep = ""), quote = FALSE, row.names = FALSE, col.names = FALSE)
 }
 
