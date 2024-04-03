79d7266817df10490093417c98f4aed913b05c49
Nicholas Horton <nicholasjhorton@gmail.com>
2024-04-03 12:01:28 -0400

    Renamed appendix and ran first set of workflow. See https://github.com/STAT325-S24/HistoryAmherstCollege/issues/30

---
+++ data-raw/data.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+history_text <- tibble(x = 1, y = 2)
+usethis::use_data(history_text, replace = TRUE)
+
+
+
+history_anno <- tibble(x = 1, y = 2) # this is a stub: needs to be replaced with appropriate 
+# calls to cleanNLP
+usethis::use_data(history_anno, replace = TRUE)
